48fcf509255becdcbcc693892fa384f6ebcc1af8
flemming <flemming@3669b85a-d59c-11de-9d39-d3ca9228641c>
2009-11-20 06:42:17 +0000

    Checking in what working code I have.

---
+++ trunk/NotSoSirius/lib/Plugins/NotSoSirius/Plugin.pm
@@ -0,0 +1,300 @@
+package Plugins::NotSoSirius::Plugin;
+
+# Copyright 2009 Robert Flemming (flemming@spiralout.net)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+use strict;
+use warnings;
+
+use HTML::Entities;
+
+use Slim::Player::ProtocolHandlers;
+use Slim::Utils::Log;
+use Slim::Utils::Prefs;
+use Slim::Utils::Strings qw(string);
+
+use Plugins::NotSoSirius::Settings;
+
+use Sirius::Sirius;
+
+use base qw(Slim::Plugin::OPMLBased);
+
+my $prefs = preferences('plugin.notsosirius');
+
+my $log = Slim::Utils::Log->addLogCategory({
+  'category'     => 'plugin.notsosirius',
+  'defaultLevel' => 'DEBUG',
+  'description'  => getDisplayName(),
+});
+
+# Maintain our Sirius instance globally
+my $sirius;
+
+# genreMap maps genre's to an array of channel keys, channelMap maps
+# channel keys to channel objects;
+my ($genreMap, $channelMap);
+
+# When using OPML 'image' items must be fully qualified URLs for images
+# to appear on the duet controller.  So we need to figure our IP and
+# port.  Theoretically Slim::Utils::IPDetect::IP_port() does this, but
+# it wasn't returning the port number.
+my $ip_port = Slim::Utils::IPDetect::IP() . ':' .  
+                preferences('server')->get('httpport');
+
+sub initPlugin {
+  my $class = shift;
+
+  Plugins::NotSoSirius::Settings->new;
+
+  Slim::Player::ProtocolHandlers->registerHandler(
+      sirius => 'Plugins::NotSoSirius::ProtocolHandler'
+  );
+
+  my $accountRef = $prefs->get('account') || '';
+  if ($accountRef) {
+    my $account = @{$accountRef}[0];
+    $sirius = Sirius->new($account);
+    $sirius->debug(1);
+    $sirius->auth();
+    ($genreMap, $channelMap) = {};
+  } else {
+    $sirius->{error} = "Plugin not configured"
+  }
+
+  $class->SUPER::initPlugin(
+      tag            => 'notsosirius',
+      menu           => 'radios',
+      weight         => 50,
+  );
+
+  if ( main::SLIM_SERVICE ) {
+    my $menu = {
+      useMode => sub { $class->setMode(@_) },
+      header  => string('PLUGIN_NOTSOSIRIUS'),
+    };
+
+    Slim::Buttons::Home::addSubMenu(
+        'RADIO',
+        'PLUGIN_NOTSOSIRIUS',
+        $menu,
+    );
+
+    $class->initCLI(
+        tag  => 'notsosirius_radio',
+        menu => 'radios',
+    );
+  }
+}
+
+sub feed {
+  my $class = shift;
+
+  my @opml;
+  if ($sirius->{error}) {
+    @opml = ($class->Error());
+  } else {
+    # Go through the slow painful process of building the genre and
+    # channel maps if we don't already have them
+    if (! ($genreMap && $channelMap)) {
+      ($genreMap, $channelMap) = &getGenreChannelMap();
+    }
+
+    push(@opml, {
+      'name' => 'By Channel Name',
+      'type' => 'opml',
+      'items' => [&OPMLByName()],
+    }, {
+      'name' => 'By Channel Number',
+      'type' => 'opml',
+      'items' => [&OPMLByNumber()],
+    });
+    @opml = (@opml, &OPMLByGenre());
+  }
+
+  # Dance around button mode stuff.  I don't really understand it, but
+  # it works so woohoo.
+  my $caller = (caller(1))[3];
+  if ($caller =~ /setMode/) {
+    return sub { $_[1]->(\@opml) };
+  } else {
+    return { title => string('PLUGIN_NOTSOSIRIUS'), items => \@opml };
+  }
+}
+
+sub Error {
+  my $class = shift;
+  my $error = shift;
+
+  # First use the internal NotSoSirius message, then the user specified one
+  # so that we can catch all errors with a single test while browsing.
+  if ($sirius->{error}) {
+    $error = $sirius->{error};
+  } elsif (! $error) {
+    $error = "Unknown error";
+  }
+
+  # Log the error and return it is a feed item.  Clicking it won't do
+  # much, but it's the only way to return the error
+  $log->info($error);
+
+  return {'name' => $error};
+}
+
+sub getDisplayName {
+  return 'PLUGIN_NOTSOSIRIUS';
+}
+
+sub playerMenu () {
+  return 'RADIO';
+}
+
+sub getStream {
+  my $client = shift;
+  my $key = shift;
+  my $callback = shift;
+  my $song = shift;
+ 
+  my $channel = $channelMap->{$key};
+
+  # Only the US site requires all of these, the Canada site only needs
+  # the channel key value, the others are ignored
+  my $stream = $sirius->getStream(
+    $channel->{key}, $channel->{genre}, $channel->{category});
+
+  $stream =~ s/^http/mms/;
+  $log->debug("Streaming: $stream\n");
+
+  if ($::VERSION lt '7.4') {
+    $song->{'streamUrl'} = $stream;
+    $song->{'wmaMetadataStream'} = 2;
+  } else {
+    $song->streamUrl($stream);
+    $song->wmaMetadataStream(2);
+  }
+
+  $callback->();
+  return;
+}
+
+sub getGenreChannelMap {
+  my (%genreMap, %channelMap);
+
+  my %categories = $sirius->getCategories();
+  foreach my $c (keys %categories) {
+    # Convert the category name to 'title' case
+    (my $category = $categories{$c}) =~ s/(\w+)/\u\L$1/g;
+
+    # We want to colapse the 3 tier menu system on the Sirius website into a 2
+    # tier menu system by pulling the genres under the 'MUSIC' category up a
+    # level.  @category_channels are essentially the non-music channels, while
+    # @genre_channels are the music channels which fall under each genre.
+
+    my @category_channels;
+    my %genres = $sirius->getGenres($c);
+    foreach my $g (keys %genres) {
+      my $genre = $genres{$g};
+
+      my @genre_channels;
+      my %channels = $sirius->getChannels($c, $g);
+      foreach my $n (keys %channels) {
+        my $channel = $channels{$n};
+        $channel->{key} = $n;
+        # Add the category and genre keys as attributes
+        $channel->{category} = $c;
+        $channel->{genre} = $g;
+        $channelMap{$n} = $channel;
+        push(@genre_channels, $n);
+      }
+      if ($category eq 'Music') {
+        # Group the music channels by genre
+        $genreMap{$genre} = \@genre_channels;
+      } else {
+        @category_channels = (@genre_channels, @category_channels);
+      }
+    }
+    # Group the non-music channels by category
+    if ($category ne 'Music') {
+    	$genreMap{$category} = \@category_channels;
+    }
+  }
+  return (\%genreMap, \%channelMap);
+}
+
+sub getChannelImage {
+  my $channel = shift;
+
+  return 'http://' . $ip_port . 
+         '/plugins/NotSoSirius/html/images/' . $channel . '.png';
+}
+
+sub OPMLByNumber {
+  my @opml;
+  my %channelMap = %{$channelMap};
+
+  foreach my $key (keys %channelMap) {
+    my $channel = $channelMap{$key};
+    push(@opml, {
+      'name' => $channel->{number}.'. '.$channel->{name},
+      'description' => $channel->{desc},
+      'type' => 'audio',
+      'image' => &getChannelImage($channel->{key}),
+      'url' => 'sirius://' . $channel->{key},
+    });
+  }
+  return sort { (split('\.', $a->{name}))[0] <=> (split('\.', $b->{name}))[0] } @opml;
+}
+
+sub OPMLByName {
+  my @opml;
+  my %channelMap = %{$channelMap};
+
+  foreach my $key (keys %channelMap) {
+    my $channel = $channelMap{$key};
+    push(@opml, {
+      'name' => $channel->{name},
+      'description' => $channel->{desc},
+      'type' => 'audio',
+      'image' => &getChannelImage($channel->{key}),
+      'url' => 'sirius://' . $channel->{key},
+    });
+  }
+  return sort { $a->{name} cmp $b->{name} } @opml;
+}
+
+sub OPMLByGenre {
+  my @opml;
+
+  my %genreMap = %{$genreMap};
+  my %channelMap = %{$channelMap};
+
+  foreach my $genre (keys %genreMap) {
+    my @channels;
+    foreach my $key (@{$genreMap{$genre}}) {
+      my $channel = $channelMap{$key};
+      push(@channels, {
+        'name' => $channel->{number}.'. '.$channel->{name},
+        'description' => $channel->{desc},
+        'type' => 'audio',
+        'image' => &getChannelImage($channel->{key}),
+        'url' => 'sirius://' . $channel->{key},
+      });  
+    }
+    @channels = sort { (split('\.', $a->{name}))[0] <=> (split('\.', $b->{name}))[0] } @channels;
+    push(@opml, {
+      'name' => $genre,
+      'type' => 'opml',
+      'items' => \@channels,
+    });
+  }
+  return sort { $a->{name} cmp $b->{name} } @opml;
+}
+
+1;
---
+++ trunk/NotSoSirius/lib/Plugins/NotSoSirius/ProtocolHandler.pm
@@ -0,0 +1,197 @@
+package Plugins::NotSoSirius::ProtocolHandler;
+
+# $Id$
+
+use strict;
+use base qw(Slim::Player::Protocols::MMS);
+
+use Slim::Utils::Log;
+use Slim::Utils::Misc;
+
+my $log = logger('plugin.notsosirius');
+
+sub audioScrobblerSource { 'R' }
+
+sub getFormatForURL { 'wma' }
+
+sub isAudioURL { 1 }
+
+sub isRemote { 1 }
+
+# Support transcoding
+sub new {
+	my $class = shift;
+	my $args  = shift;
+
+  my $streamUrl;
+  if ($::VERSION lt '7.4') {
+    $streamUrl = $args->{'song'}->{'streamUrl'};
+  } else {
+    $streamUrl = $args->{'song'}->streamUrl();
+  }
+
+	my $url = $streamUrl;
+	
+	return unless $url;
+	
+	$args->{'url'} = $url;
+
+	return $class->SUPER::new($args);
+}
+
+sub getNextTrack {
+	my ($class, $song, $successCb, $errorCb) = @_;
+	
+	if ( main::SLIM_SERVICE ) {
+		# Fail if firmware doesn't support metadata
+		my $client = $song->master();
+		my $old;
+		
+		my $deviceid = $client->deviceid;
+		my $rev      = $client->revision;
+		
+		if ( $deviceid == 4 && $rev < 119 ) {
+			$old = 1;
+		}
+		elsif ( $deviceid == 5 && $rev < 69 ) {
+			$old = 1;
+		}
+		elsif ( $deviceid == 7 && $rev < 54 ) {
+			$old = 1;
+		}
+		elsif ( $deviceid == 10 && $rev < 39 ) {
+			$old = 1;
+		}
+		
+		if ( $old ) {
+			$errorCb->('PLUGIN_NOTSOSIRIUS_FIRMWARE_UPGRADE_REQUIRED');
+			return;
+		}
+	}
+	
+	my ($channelId) = $song->currentTrack()->url =~ m{^sirius://(.+)};
+        Plugins::NotSoSirius::Plugin::getStream($song->master(), $channelId, $successCb, $song);
+}
+
+sub canDirectStreamSong {
+	my ( $class, $client, $song ) = @_;
+
+  my $streamUrl;
+  if ($::VERSION lt '7.4') {
+    $streamUrl = $song->{'streamUrl'};
+  } else {
+    $streamUrl = $song->streamUrl();
+  }
+
+  return $class->SUPER::canDirectStream($client, $streamUrl, $class->getFormatForURL());
+
+}
+
+sub parseDirectHeaders {
+	my $class   = shift;
+	my $client  = shift || return;
+	my $url     = shift;
+#	my @headers = @_;
+	
+	my $contentType = 'wma';
+	my $bitrate     = $client->streamingSong()->bitrate();
+
+	# title, bitrate, metaint, redir, type, length, body
+	return (undef, $bitrate, 0, undef, $contentType, undef, undef);
+}
+
+sub parseMetadata {
+	my ( $class, $client, $song, $metadata ) = @_;
+	
+	# If we have ASF_Command_Media, process it here, otherwise let parent handle it
+	my $guid;
+	map { $guid .= $_ } unpack( 'H*', substr $metadata, 0, 16 );
+	
+	if ( $guid ne '59dacfc059e611d0a3ac00a0c90348f6' ) { # ASF_Command_Media
+		return $class->SUPER::parseMetadata( $client, $song, $metadata );
+	}
+	
+	substr $metadata, 0, 24, '';
+		
+	# Format of the metadata stream is:
+	# TITLE <title>|ARTIST <artist>\0
+	
+	# WMA text is in UTF-16, if we can't decode it, just wait for more data
+	# Cut off first 24 bytes (16 bytes GUID and 8 bytes object_size)
+	$metadata = eval { Encode::decode( 'UTF-16LE', $metadata ) } || return;
+	
+	#$log->debug( "ASF_Command_Media: $metadata" );
+	
+	my ($artist, $title);
+	
+	if ( $metadata =~ /TITLE\s+([^|]+)/ ) {
+		$title = $1;
+	}
+	
+	if ( $metadata =~ /ARTIST\s([^\0]+)/ ) {
+		$artist = $1;
+	}
+	
+	if ( $artist || $title ) {
+		if ( $artist && $artist ne $title ) {
+			$title = "$artist - $title";
+		}
+		
+		# Delay the title by the length of the output buffer only
+		Slim::Music::Info::setDelayedTitle( $song->master(),  $song->currentTrack()->url, $title, 'output-only' );
+	}
+	
+	return;
+}
+
+# Metadata for a URL, used by CLI/JSON clients
+sub getMetadataFor {
+	my ( $class, $client, $url ) = @_;
+
+	my ($artist, $title);
+	# Return artist and title if the metadata looks like Artist - Title
+	if ( my $currentTitle = Slim::Music::Info::getCurrentTitle( $client, $url ) ) {
+		my @dashes = $currentTitle =~ /( - )/g;
+		if ( scalar @dashes == 1 ) {
+			($artist, $title) = split / - /, $currentTitle;
+		}
+
+		else {
+			$title = $currentTitle;
+		}
+	}
+
+	# try to find song
+	my $song = $client->streamingSong();
+
+	my $bitrate;
+	my $logo;
+
+  my $streamUrl;
+  if ($::VERSION lt '7.4') {
+    $streamUrl = $song->{'streamUrl'};
+  } else {
+    $streamUrl = $song->streamUrl();
+  }
+
+	if ($song && ($song->currentTrack()->url eq $url || $streamUrl eq $url)) {
+		$bitrate = ($song->bitrate || 0) / 1000;
+		my ($channelId) = $url =~ m{^sirius://(.+)};
+		$logo = Plugins::NotSoSirius::Plugin::getChannelImage($channelId);
+	}
+	$bitrate ||= 128;
+	$logo ||= $class->getIcon($url);
+	return {
+		artist  => $artist,
+		title   => $title,
+		cover   => $logo,
+		bitrate => $bitrate . 'k CBR',
+		type    => 'WMA (Sirius)',
+	};
+}
+
+sub getIcon {
+	return Plugins::NotSoSirius::Plugin->_pluginDataFor('icon');
+}
+
+1;
